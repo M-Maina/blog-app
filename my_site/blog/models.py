86e91b58252865b1cbd0435549925a41647ab156
wamah <pythondeveloper21@gmail.com>
2023-03-13 21:56:48 +0000

    Added pillow and settings for images upload

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -27,7 +27,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "posts", null=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -124,6 +124,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
