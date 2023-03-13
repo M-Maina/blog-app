22b65318c237f233a96c0ce17a6383966571db2e
wamah <pythondeveloper21@gmail.com>
2023-03-13 23:20:33 +0000

    pip freeze > requirement.txt

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+#ENSURE YOU HAVE CREATED SUPERUSER FOR DATABASE SCHEMA
